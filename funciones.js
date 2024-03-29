ee638f7097670c69fc1a3c31d88730c97d49a334
chalo <lpalaciosayala@gmail.com>
2024-03-28 19:04:31 -0500

    video9 curso gas avanzado

--- funciones.js
+++ funciones.js
@@ -7,3 +7,10 @@ function obtenerDatosHTML(nombre)
 {
     return HtmlService.createHtmlOutputFromFile(nombre).getContent();
 }
+
+function obtenerContactos() 
+{
+    let hoja = SpreadsheetApp.openById('1lTLRPoj5PhYbvk8mZDK8LKlsr4SUWFiUA61yurUoc5c').getActiveSheet();
+    let datos = hoja.getDataRange().getValues(); 
+    return datos;
+}
