ee08e0f5f2011251dd36d932cf31862688a5f52b
chalo <lpalaciosayala@gmail.com>
2024-03-31 11:52:34 -0500

    video15 curso gas-avanzado

--- funciones.js
+++ funciones.js
@@ -1,5 +1,15 @@
-function doGet()
+
+const HOJA = SpreadsheetApp.openById('1lTLRPoj5PhYbvk8mZDK8LKlsr4SUWFiUA61yurUoc5c').getActiveSheet();
+
+function doGet(datos)
 {
+  insertarContacto(datos.parameter.nombre, datos.parameter.correo);
+    return HtmlService.createTemplateFromFile('web').evaluate().setTitle('Agenda Google Apps Script');
+}
+
+function doPost(datos)
+{
+    insertarContacto(datos.parameter.nombre, datos.parameter.correo);
     return HtmlService.createTemplateFromFile('web').evaluate().setTitle('Agenda Google Apps Script');
 }
 
@@ -10,7 +20,10 @@ function obtenerDatosHTML(nombre)
 
 function obtenerContactos() 
 {
-    let hoja = SpreadsheetApp.openById('1lTLRPoj5PhYbvk8mZDK8LKlsr4SUWFiUA61yurUoc5c').getActiveSheet();
-    let datos = hoja.getDataRange().getValues(); 
-    return datos;
+    return HOJA.getDataRange().getValues();
+}
+
+function insertarContacto(nombre, correo)
+{
+    HOJA.appendRow([nombre,correo]);
 }
